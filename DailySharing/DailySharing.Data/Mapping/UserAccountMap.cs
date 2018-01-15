6ffe5be16be01ea4eafc4b520e4113b77f3e2781
MaYanYan <1120307922@qq.com>
2018-01-15 16:45:57 +0800

    底层

---
+++ DailySharing/DailySharing.Data/Mapping/UserAccountMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailySharing.Data.Mapping
+{
+    public class UserAccountMap : EntityTypeConfiguration<UserAccount>
+    {
+        public UserAccountMap()
+        {
+            this.HasKey(p => p.UserID)
+                .Ignore(p => p.Id);
+            this.Property(p => p.UserID).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+    }
+}
